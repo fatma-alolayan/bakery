aea07a2a7a3e1229324c2b198734dd0fd810f3d3
fatma-alolayan <temaalolayan@hotmail.com>
2020-06-29 18:03:58 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from "react";
-const styles = {
-  text: {
-    textAlign: "center",
-  },
-  shopImage: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "20%",
-  },
-  plainImage: {
-    width: "200px",
-    height: "200px",
-    border: "2px solid #555",
-  },
-  list: {
-    alignItems: "center",
-    justifyContent: "center",
-    display: "flex",
-    textAlign: "center",
-  },
-  betweenImage: {
-    marginLeft: "150px",
-  },
-};
+import items from "./items";
+import styles from "./styles";
+
 function App() {
+  const bakeryList = items.map((item) => (
+    <div style={styles.list} key={item.id}>
+      <img style={styles.plainImage} alt={item.name} src={item.image} />
+      <p style={styles.text}> {item.name} </p>
+      <p style={styles.text}> {item.price} KD</p>
+    </div>
+  ));
   return (
-    <div className="App">
+    <div>
       <div>
         <h1 style={styles.text}>Bakery</h1>
         <img
@@ -35,38 +20,7 @@ function App() {
           style={styles.shopImage}
         />
       </div>
-      <br />
-      <br />
-      <br />
-      <div style={styles.list}>
-        <div>
-          <img
-            src="https://millwayfoods.com/wp-content/uploads/2017/03/Plain-Croissant-800x600.jpg"
-            alt="plain croissant"
-            style={styles.plainImage}
-          />
-          <p>Plain Croissant </p>
-          <p>1 KD</p>
-        </div>
-        <div style={styles.betweenImage}>
-          <img
-            src="https://www.womanscribbles.net/wp-content/uploads/2017/07/cream-cheese-croissants-7.jpg"
-            alt="cheese croissant"
-            style={styles.plainImage}
-          />
-          <p>cheese Croissant </p>
-          <p>1.500 KD</p>
-        </div>
-        <div style={styles.betweenImage}>
-          <img
-            src="https://images.deliveryhero.io/image/talabat/MenuItems/Mini_Zaatar_Croissant_637031804113079514.jpg"
-            alt="zaatar croissant"
-            style={styles.plainImage}
-          />
-          <p>Zaatar Croissant </p>
-          <p>1.500 KD</p>
-        </div>
-      </div>
+      <div> {bakeryList}</div>
     </div>
   );
 }
---
+++ src/items.js
@@ -0,0 +1,25 @@
+const items = [
+  {
+    id: 1,
+    name: "Plain Croissant",
+    price: 1,
+    image:
+      "https://millwayfoods.com/wp-content/uploads/2017/03/Plain-Croissant-800x600.jpg",
+  },
+  {
+    id: 2,
+    name: "Cheese Croissant",
+    price: 1.5,
+    image:
+      "https://www.womanscribbles.net/wp-content/uploads/2017/07/cream-cheese-croissants-7.jpg",
+  },
+  {
+    id: 3,
+    name: "Zaatar Croissant",
+    price: 1.5,
+    image:
+      "https://images.deliveryhero.io/image/talabat/MenuItems/Mini_Zaatar_Croissant_637031804113079514.jpg",
+  },
+];
+
+export default items;
