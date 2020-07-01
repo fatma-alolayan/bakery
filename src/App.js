ffaf6a5563183bb58c0070e9aa234966d5f75685
fatma-alolayan <temaalolayan@hotmail.com>
2020-07-01 17:19:57 +0300

    Theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
 import styles, { GlobalStyle } from "./styles";
 import BakeryList from "./components/BakeryList";
-import { Description, ShopImage, Title } from "./styles";
+import { Description, ShopImage, Title, ThemeButton } from "./styles";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
-  mainColor: "#242424", // main font color
-  backgroundColor: "#f6d2d6", // main background color
-  black: "#0e1111",
+  light: {
+    mainColor: "#242424", // main font color
+    backgroundColor: "#f6d2d6", // main background color
+    black: "#0e1111",
+  },
+  dark: {
+    mainColor: "#f6d2d6", // main font color
+    backgroundColor: "#242424", // main background color
+    black: "#f6d2d6",
+  },
 };
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
 
+function App() {
+  let [currentTheme, setCurrentTheme] = useState("light");
+  const toggleTheme = () =>
+    setCurrentTheme(currentTheme === "light" ? "dark" : "light");
+
+  return (
+    <ThemeProvider theme={theme[currentTheme]}>
+      <GlobalStyle />
+      <ThemeButton onClick={toggleTheme}>Dark Theme</ThemeButton>
       <Title>Bakery</Title>
       <Description>Fresh</Description>
       <ShopImage
--- src/styles.js
+++ src/styles.js
@@ -36,6 +36,20 @@ const GlobalStyle = createGlobalStyle`
     background-color: ${(props) => props.theme.backgroundColor}
   }
 `;
+
+const ThemeButton = styled.button`
+   {
+    background-color: #c0babc;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+`;
+
 export {
   BakeryWrapper,
   Description,
@@ -43,4 +57,5 @@ export {
   Title,
   ShopImage,
   GlobalStyle,
+  ThemeButton,
 };
